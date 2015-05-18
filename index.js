6ca9353b435da3887976c4ad43330c0f391af96b
Steven La <mrstevenla@gmail.com>
2015-05-17 20:41:11 -0700

    works

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-#!/usr/bin/env node --harmony
+#!/usr/bin/env node
 
-var app = require('koa')();
-var router = require('koa-router')();
+var express = require('express');
 var fs = require('fs');
-
 var handlebars = require('handlebars');
+var duoApi = require('duo_api_nodejs');
 
-router.get('/', function *(next) {
-	var template = fs.readFileSync('templates/index.xml', 'utf-8');
-	var rendered = handlebars.compile(template);
-	this.body = rendered();
+var config = require('./config.json');
+
+var duoClient = new duoApi.Client(config.integrationKey, config.secretKey, config.hostname);
+var app = express();
+
+app.get('/', function (req, res) {
+	var options = {
+		'username': 'mrstevenla@gmail.com',
+		'factor': 'push',
+		'device': 'auto'
+	};
+
+	duoClient.jsonApiCall('POST', '/auth/v2/auth', options, function(duoResponse) {
+		console.log(duoResponse);
+		var template = fs.readFileSync('templates/index.xml', 'utf-8');
+		var rendered = handlebars.compile(template);
+		res.send(rendered());
+	});
 });
 
-console.log('listening on http://localhost:3000');
-app
-	.use(router.routes())
-	.listen(3000);
-;
+app.use('/static', express.static('static'));
+
+var server = app.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Listening at http://%s:%s', host, port)
+});
