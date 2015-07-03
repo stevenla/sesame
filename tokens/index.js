2a4739a7fdf875c70888e6a42e0e9e00572e490d
Steven La <mrstevenla@gmail.com>
2015-07-03 19:58:01 -0400

    white listing but currently turned off

--- tokens/index.js
+++ tokens/index.js
@@ -1,3 +1,4 @@
+var config = require('../config.json');
 var TokenStore = require('./token-store');
 var Router = require('express').Router;
 
@@ -8,27 +9,32 @@ var router = new Router();
 router.all('/create', function (req, res) {
     var username = req.query.username;
     var token = tokens.create(username);
+    // TODO: Enable whitelisting by making it more secure than just sending a get request with username param set
+    /*
+    if (config.userWhitelist.indexOf(username) < 0) {
+        console.log('User outside of whitelist attempted to create token: %s', username);
+        return res.send(null);
+    }
+    */
 
     var response = {
-		'success': true,
-		'token': token
+        'success': true,
+        'token': token
     }
 
     console.log('Created Token: %j', token);
-
-	res.send(response);
+    res.send(response);
 });
 
 router.all('/consume', function (req, res) {
     var token = tokens.consume();
-
-	if (token) {
-		console.log('Consumed Token: %j', token);
-		res.render('success');
-	} else {
-		console.log('Failed to consume');
-		res.status(401).end();
-	}
+    if (token) {
+        console.log('Consumed Token: %j', token);
+        res.render('success');
+    } else {
+        console.log('Failed to consume');
+        res.status(401).end();
+    }
 });
 
 module.exports = router;
