1af99570d855c403270d66970ce73046b928357b
Steven La <sla@box.com>
2015-07-04 15:29:26 -0700

    spaces back to tabs

--- tokens/index.js
+++ tokens/index.js
@@ -7,34 +7,34 @@ var tokens = new TokenStore();
 var router = new Router();
 
 router.all('/create', function (req, res) {
-    var username = req.query.username;
-    var token = tokens.create(username);
-    // TODO: Enable whitelisting by making it more secure than just sending a get request with username param set
-    /*
-    if (config.userWhitelist.indexOf(username) < 0) {
-        console.log('User outside of whitelist attempted to create token: %s', username);
-        return res.send(null);
-    }
-    */
+	var username = req.query.username;
+	var token = tokens.create(username);
+	// TODO: Enable whitelisting by making it more secure than just sending a get request with username param set
+	/*
+	if (config.userWhitelist.indexOf(username) < 0) {
+		console.log('User outside of whitelist attempted to create token: %s', username);
+		return res.send(null);
+	}
+	*/
 
-    var response = {
-        'success': true,
-        'token': token
-    }
+	var response = {
+		'success': true,
+		'token': token
+	}
 
-    console.log('Created Token: %j', token);
-    res.send(response);
+	console.log('Created Token: %j', token);
+	res.send(response);
 });
 
 router.all('/consume', function (req, res) {
-    var token = tokens.consume();
-    if (token) {
-        console.log('Consumed Token: %j', token);
-        res.render('success');
-    } else {
-        console.log('Failed to consume');
-        res.status(401).end();
-    }
+	var token = tokens.consume();
+	if (token) {
+		console.log('Consumed Token: %j', token);
+		res.render('success');
+	} else {
+		console.log('Failed to consume');
+		res.status(401).end();
+	}
 });
 
 module.exports = router;
